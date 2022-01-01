78b3474fc5f1b63de771a6d7a3050f7ede299860
Leon <mallorystammer2@gmail.com>
2021-12-31 17:46:57 -0700

    added Event Model class, added Date field

--- CodingEvents/Controllers/EventsController.cs
+++ CodingEvents/Controllers/EventsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CodingEvents.Models;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -11,7 +12,8 @@ namespace CodingEvents.Controllers
     public class EventsController : Controller
     {
         //static private List<string> Events = new List<string>();
-        static private Dictionary<string, string> Events = new Dictionary<string, string>();
+        //static private Dictionary<string, string> Events = new Dictionary<string, string>();
+        static private List <Event> Events = new List<Event>();
 
         // GET: /<controller>/
         [HttpGet]
@@ -33,9 +35,9 @@ namespace CodingEvents.Controllers
 
         [HttpPost]
         [Route("/Events/Add")]
-        public IActionResult NewEvent(string name, string description)
+        public IActionResult NewEvent(string name, string description, DateTime date)
         {
-            Events.Add(name, description);
+            Events.Add(new Event(name, description, date));
 
             return Redirect("/Events");
         }
---
+++ CodingEvents/Models/Event.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace CodingEvents.Models
+{
+    public class Event
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public Event(string name, string description, DateTime date)
+        {
+            Name = name;
+            Description = description;
+            Date = date; 
+        }
+
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
